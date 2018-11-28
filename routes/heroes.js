1aaf068363b5c1e768e8d0aab1bf38e47ead4045
Logan Wang <loganxwang@hotmail.com>
2018-11-28 06:56:01 -0800

    added matchups endpoint

--- routes/heroes.js
+++ routes/heroes.js
@@ -6,7 +6,18 @@ const axios = require('axios');
 router.get('/herostats', (req, res) => {
   axios.get('https://api.opendota.com/api/herostats')
     .then(response => {
-      res.json(response.data);
+      const heroes = {
+        int: [],
+        str: [],
+        agi: []
+      };
+
+      for (let i = 0; i < response.data.length; i++) {
+        const item = response.data[i];
+        heroes[item.primary_attr].push(item);
+      }
+      res.json(heroes);
+      //console.log(heroes);
     }).catch(err => console.log(err));
 });
 
@@ -14,7 +25,42 @@ router.get('/heroes/:id/matchups', (req, res) => {
   const { id } = req.params;
   axios.get(`https://api.opendota.com/api/heroes/${id}/matchups`)
     .then(response => {
-      res.json(response.data);
+
+      response.data; //all the data
+      let high_hero_id = 0;
+      let low_hero_id = 0;
+      let highWinRate = 0;
+      let lowWinRate = 100;
+
+      //finding highest win rate
+      for (let i = 0; i < response.data.length; i++) {
+
+        const item = response.data[i];
+        if (item.games_played > 15) {
+          let winRate = item.wins / item.games_played;
+          if (winRate > highWinRate) {
+            highWinRate = winRate;
+            high_hero_id = item.hero_id;
+          }
+        }
+      }
+      //finding lowest winrate
+      for (let i = 0; i < response.data.length; i++) {
+
+        const item = response.data[i];
+        if (item.games_played > 15) {
+          let winRate = item.wins / item.games_played;
+          if (winRate < lowWinRate) {
+            lowWinRate = winRate;
+            low_hero_id = item.hero_id;
+          }
+        }
+      }
+      let highestAndLowestWinRate = [{ highwinrateid: high_hero_id, lowwinrateid: low_hero_id }];
+      // console.log(highestAndLowestWinRate);
+
+
+      res.json(highestAndLowestWinRate);
     }).catch(err => console.log(err));
 });
 
