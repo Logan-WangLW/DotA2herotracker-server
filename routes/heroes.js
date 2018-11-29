68945a706ef7ffa8a0d864e9428597643a157705
Logan Wang <loganxwang@hotmail.com>
2018-11-28 22:13:28 -0800

    updated server

--- routes/heroes.js
+++ routes/heroes.js
@@ -3,42 +3,64 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-router.get('/herostats', (req, res) => {
-  axios.get('https://api.opendota.com/api/herostats')
-    .then(response => {
-      const heroes = {
-        int: [],
-        str: [],
-        agi: []
-      };
+const Heroes = require('../models/heroes');
 
-      for (let i = 0; i < response.data.length; i++) {
-        const item = response.data[i];
-        heroes[item.primary_attr].push(item);
-      }
-      res.json(heroes);
-      //console.log(heroes);
-    }).catch(err => console.log(err));
+router.get('/herostats', async (req, res) => {
+  // axios
+  //   .get('https://api.opendota.com/api/herostats')
+  //   .then(async response => {
+  // await Heroes.collection.insert(response.data)
+
+  // console.log('heroes bulk done')
+
+  const int = await Heroes.find({ primary_attr: 'int' });
+  const agi = await Heroes.find({ primary_attr: 'agi' });
+  const str = await Heroes.find({ primary_attr: 'str' });
+
+  const heroes = {
+    int,
+    str,
+    agi,
+  };
+
+  // for (let i = 0; i < response.data.length; i++) {
+  //   const item = response.data[i];
+  //   heroes[item.primary_attr].push(item);
+  // }
+  res.json(heroes);
+  //console.log(heroes);
+  // })
+  // .catch(err => console.log(err));
 });
 //get single hero
 router.get('/heroes/:id', (req, res) => {
-  axios.get('https://api.opendota.com/api/herostats')
-    .then(response => {
-      const hero = response.data.find(el => el.id === parseInt(req.params.id));
-      //console.log(hero);
+  const id = parseInt(req.params.id);
+  Heroes.findOne({ id })
+    .then(hero => {
       if (hero) {
         res.json(hero);
       } else {
         res.sendStatus(404);
       }
-    });
+    })
+    .catch(err => console.error(err));
+  // axios.get('https://api.opendota.com/api/herostats')
+  //   .then(response => {
+  //     const hero = response.data.find(el => el.id === parseInt(req.params.id));
+  //     //console.log(hero);
+  //     if (hero) {
+  //       res.json(hero);
+  //     } else {
+  //       res.sendStatus(404);
+  //     }
+  //   });
 });
 
 router.get('/heroes/:id/matchups', (req, res) => {
   const { id } = req.params;
-  axios.get(`https://api.opendota.com/api/heroes/${id}/matchups`)
+  axios
+    .get(`https://api.opendota.com/api/heroes/${id}/matchups`)
     .then(response => {
-
       response.data; //all the data
       let high_hero_id = 0;
       let low_hero_id = 0;
@@ -47,7 +69,6 @@ router.get('/heroes/:id/matchups', (req, res) => {
 
       //finding highest win rate
       for (let i = 0; i < response.data.length; i++) {
-
         const item = response.data[i];
         if (item.games_played > 15) {
           let winRate = item.wins / item.games_played;
@@ -59,7 +80,6 @@ router.get('/heroes/:id/matchups', (req, res) => {
       }
       //finding lowest winrate
       for (let i = 0; i < response.data.length; i++) {
-
         const item = response.data[i];
         if (item.games_played > 15) {
           let winRate = item.wins / item.games_played;
@@ -69,12 +89,14 @@ router.get('/heroes/:id/matchups', (req, res) => {
           }
         }
       }
-      let highestAndLowestWinRate = [{ highwinrateid: high_hero_id, lowwinrateid: low_hero_id }];
+      let highestAndLowestWinRate = [
+        { highwinrateid: high_hero_id, lowwinrateid: low_hero_id },
+      ];
       // console.log(highestAndLowestWinRate);
 
-
       res.json(highestAndLowestWinRate);
-    }).catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
